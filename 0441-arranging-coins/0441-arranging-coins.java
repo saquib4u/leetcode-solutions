7639a2753efb571b938e15f232a941ee776f2ac9
Md Saquib Alam <63180851+saquib4u@users.noreply.github.com>
2022-11-19 16:49:19 +0530

    Time: 8 ms (45.24%), Space: 41.2 MB (70.40%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,13 +1,24 @@
 class Solution {
     public int arrangeCoins(int n) {
-        long sum = 1;
-        int i = 1;
         
-        while(sum<=n){
-           if(sum == n)return i;
-            i++;
-            sum += i;
-        }
-        return i-1;
+        //1st approach
+//         long sum = 1;
+//         int i = 1;
+        
+//         while(sum<=n){
+//            if(sum == n)return i;
+//             i++;
+//             sum += i;
+//         }
+//         return i-1;
+        
+      //2nd approcah
+        
+      int i = 1;
+      while(n >= i){
+          n = n-i;
+          i++;
+      }
+      return i-1;
     }
 }
