14b0e41a7d2a49898a34c4d517219005d9a9b931
Md Saquib Alam <63180851+saquib4u@users.noreply.github.com>
2022-12-26 15:49:19 +0530

    Added solution - LeetHub

---
+++ Sum of all divisors from 1 to n - GFG/sum-of-all-divisors-from-1-to-n.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+			
+            
+            Solution ob = new Solution();
+            long ans  = ob.sumOfDivisors(N);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static long sumOfDivisors(int N){
+        // code here
+        long ans=0L;
+
+        //BRUTE FORCE APPROACH
+        // for(int i =1; i<=N; i++){
+        //     for(int j=1; j<=i; j++){
+        //         if(i%j == 0){
+        //             ans += j;
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        
+        //OPTIMIZED APPROACH
+        // for(int i =1; i<=N; i++){
+        //     for(int j =1; j<=(int)Math.sqrt(i);j++){
+        //         if(i%j==0){
+        //             // ans += j;
+        //             // if(j != i/j){
+        //             //     ans += i/j;
+        //             // }
+        //             if (i / j == j)
+        //                 ans += j;
+        //             else
+        //                 ans += j + i / j;
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        //
+        for (int i = 1; i <= N; ++i)
+            ans += (N / i) * i;
+        return ans;
+    }
+}
