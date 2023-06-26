71c251447618675f0b0c932e53e633697bac52b1
Md Saquib Alam <63180851+saquib4u@users.noreply.github.com>
2023-06-26 11:49:35 +0530

    Added solution - LeetHub

--- Merge Sort - GFG/merge-sort.java
+++ Merge Sort - GFG/merge-sort.java
@@ -53,44 +53,6 @@ class Merge_Sort
 
 
 // } Driver Code Ends
-
-
-// class Solution {
-//     void merge(int arr[], int l, int mid, int r) {
-//         int i = l, j = mid+1, k = 0;
-//         int temp[] = new int[1000];
-        
-//         while (i <= mid && j <= r) {
-//             if (arr[i] < arr[j]) {
-//                 temp[k++] = arr[i++];
-//             } else {
-//                 temp[k++] = arr[j++];
-//             }
-//         }
-        
-//       while (i <= mid) {
-//             temp[k++] = arr[i++];
-//         }
-
-//         while (j <= r) {
-//             temp[k++] = arr[j++];
-//         }
-        
-//         // Copy elements from temp array back to the original array
-//         for (k = 0; k <= r; k++) {
-//             arr[k] = temp[k];
-//         }
-//     }
-
-//     void mergeSort(int arr[], int l, int r) {
-//         if (l < r) {
-//             int mid = (l + r) / 2;
-//             mergeSort(arr, l, mid);
-//             mergeSort(arr, mid + 1, r);
-//             merge(arr, l, mid, r);
-//         }
-//     }
-// }
 class Solution {
     void merge(int arr[], int l, int mid, int r) {
         int i = l;
