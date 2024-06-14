b384af36f02f17fb32a290750f2a77f95b7dc561
Md Saquib Alam <63180851+saquib4u@users.noreply.github.com>
2024-06-14 12:17:38 +0530

    Time: 12 ms, Memory: 45.2 MB - LeetHub

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -1,12 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer, Integer> countMap = new HashMap<>();
+        HashMap<Integer, Integer> countMap = new HashMap<>();
         for (int num : nums) {
             countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
-        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-            if (entry.getValue() == 1) {
-                return entry.getKey();
+       for (int num : nums) {
+            if (countMap.get(num) == 1) {
+                return num;
             }
         }
         return 0;
