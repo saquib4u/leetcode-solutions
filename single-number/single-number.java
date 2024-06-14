48e2b09fa2097bb9e0c8817bb23ca79badb74fd1
Md Saquib Alam <63180851+saquib4u@users.noreply.github.com>
2024-06-14 12:15:34 +0530

    Time: 13 ms, Memory: 45 MB - LeetHub

---
+++ single-number/single-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return 0;
+    }
+}
